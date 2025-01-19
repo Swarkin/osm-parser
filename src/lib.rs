1ccb1d457f023eb50ea6eb3444bd7b32a2017fc9
swarkin <102416174+Swarkin@users.noreply.github.com>
2025-01-19 16:05:50 +0100

    Expose missing types

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,4 @@ pub mod types;
 pub mod convert;
 
 pub use parser::parse;
-pub use types::{Coordinate, Node, Nodes, OsmData, Way, Ways};
+pub use types::{Bounds, Coordinate, Id, Node, Nodes, OsmData, Tags, Way, Ways};
